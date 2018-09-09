002c86346e16965579b1517c7b6f6d44aa798636
myronliu347 <myronliu347@gmail.com>
2018-09-09 15:42:36 +0800

    feat: change version text

--- src/components/nav.js
+++ src/components/nav.js
@@ -29,7 +29,7 @@ export default {
             <div class='mu-app-version'>v{this.version}</div>
             <mu-list slot='content' dense>
               <mu-list-item button to={'/' + this.locale}>
-                <mu-list-item-title>v3.0.1</mu-list-item-title>
+                <mu-list-item-title>v{this.version}</mu-list-item-title>
               </mu-list-item>
               <mu-list-item button={true} href='/2.1.0'>
                 <mu-list-item-title>v2.10.0</mu-list-item-title>
